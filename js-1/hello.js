b45c389bf03797b4267783dc3d5b34979c8a2eb4
natalia270201 <natalia270201@gmail.com>
2023-03-10 19:41:25 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+function hola(Nombre){
+saludo = "Hola" +Nombre+ "!"
+return saludo
+}
+
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,9 @@
+function sum(numero){
+    gauss = numero*(numero + 1)/ 2
+    return gauss
+
+}
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
